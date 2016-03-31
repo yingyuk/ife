f4a4ef0268de33c7c95091660d0aa809c7baeaf8
iSAM2016 <1228901986@qq.com>
2016-03-31 11:04:26 +0800

    任务11

---
+++ src/main/webapp/task11/js/wangyi.js
@@ -0,0 +1,6 @@
+//modle.js
+ var deviceWidth = document.documentElement.clientWidth;
+ if( deviceWidth > 640 ) deviceWidth = 1000;
+ 
+
+ document.documentElement.style.fontSize = deviceWidth / 10 +'px';
